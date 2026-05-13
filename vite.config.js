877f831dfb89ee1e82ecb0c96df8c9fd6ec80784
HudayaW <hudayamuhammadrizqi@gmail.com>
2026-05-13 16:55:28 +0700

    edit coba

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  base: '/vitePDW_20240140124/',
+  plugins: [react(), tailwindcss()],
 })
